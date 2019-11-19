e5c6616371c25ba496fd974e8accd28879ac8932
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-11-19 21:35:24 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
 class Owner
-  # code goes here
+
+  attr_reader :name, :species
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @species
+    @@all << self
+  end 
+
+  def name
+    @name 
+  end 
+  
+  def species
+    @species = "human"
+  end 
+  
+  def say_species 
+     "I am a #{species}."
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  # def self.count 
+  #   self.each do |owner|
+  #     owner
+  #   end
+  end 
+    
 end
